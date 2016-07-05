bce9a97a8f6d2d008529d52393d9ab9a339714b4
zhulinpinyu <mulixiang199016@gmail.com>
2016-07-05 23:33:41 +0800

    splitting the configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const validator = require('webpack-validator')
 
 const PATHS = {
   app: path.join(__dirname,'app'),
   build: path.join(__dirname,'build')
 }
 
-module.exports = {
+const common = {
   entry: {
     app: PATHS.app
   },
@@ -20,3 +22,15 @@ module.exports = {
     })
   ]
 }
+
+let config
+
+switch(process.env.npm_lifecircle_event){
+  case 'build':
+    config = merge(common,{})
+    break
+  default:
+    config = merge(common,{})
+}
+
+module.exports = validator(config)
