956d609a22933d10312d5a047b0b6682118770f1
zhulinpinyu <mulixiang199016@gmail.com>
2016-07-06 00:40:59 +0800

    HMR on windows ubuntu and vagrant

--- libs/parts.js
+++ libs/parts.js
@@ -2,6 +2,12 @@ const webpack =  require('webpack')
 
 exports.devServer = function(options){
   return {
+    watchOptions: {
+      //Delay the rebuild after the first change
+      aggregateTimeout: 300,
+      // Poll using interval(in ms, accepts boolean too)
+      poll: 1000
+    },
     devServer: {
       historyApiFallback: true,
       hot: true,
