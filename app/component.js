8337c0dd23c023ae33924083a3d29146d44a5bb4
zhulinpinyu <mulixiang199016@gmail.com>
2016-07-05 22:50:30 +0800

    shortcut for run webpack

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = function(){
-  var element = document.createElement('h1')
+  let element = document.createElement('h1')
   element.innerHTML = 'Hello Webpack'
   return element
 }
--- build/app.js
+++ build/app.js
@@ -53,7 +53,7 @@
 /***/ function(module, exports) {
 
 	module.exports = function(){
-	  var element = document.createElement('h1')
+	  let element = document.createElement('h1')
 	  element.innerHTML = 'Hello Webpack'
 	  return element
 	}
